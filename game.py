e076e292188e1714d61dd653bf9eabbb83cc9564
Sedina <sat9978l@nyu.edu>
2021-07-08 22:27:05 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,11 @@
 print("Rock, Paper, Scissors, Shoot!")
 
-
-
+import random
 
 # Ask for a user input
-#SOURCE
-#s=sdks
+# SOURCE: in-class
 
-x = input("Please choose 'rock', 'paper', or scissors'")
+x = input("Please choose 'rock', 'paper', or 'scissors': ")
 print (x)
 
  # Validate user input
@@ -15,11 +13,31 @@ print (x)
 if (x == "rock") or (x == "paper") or (x == "scissors"):
     print("VALID")
 else:
-    print("Whoops! Please try again :)")
+    print("Whoops! Please make sure you enter 'rock', 'paper', or 'scissors'")
     exit()
 
 #Generate computer choice
+#SOURCE: in-class stack overflow
+
+valid_options = ["rock", "paper", "scissors"]
+
+c = random.choice(valid_options)
+
+print("You chose:", x)
+
+print("Computer chose:", c)
 
 #Determine winner
+
+#if (x == "rock") and (c == "scissors")
+    #print ("Rock beats scissors")
+#if (x == "scissors") and (c == "paper")
+    #print ("Scissors beat paper")
+#if (x == "paper") and (c == "rock")
+ #   print ("Paper beats rock")
+
+#if (c == "rock") and (x == "scissors)
+#if (c == "scissors") and (x == "paper")
+#if (c == "paper") and (x == "rock")
 
  #Display final results
