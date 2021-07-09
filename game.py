59ad6336a6555b2de9c88b6e8cd7dc70f7dc4e2d
Sedina <sat9978l@nyu.edu>
2021-07-08 20:36:09 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+
+# Ask for a user input
+#SOURCE
+#s=sdks
+
+x = input("Please choose 'rock', 'paper', or scissors'")
+print (x)
+
+ # Validate user input
+
+#Generate computer choice
+
+#Determine winner
+
+
+ #Display final results
